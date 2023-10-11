de69702624e0f12979cb3a1e28b43f8d9289706c
dengjian <dengjian@rivergame.net>
2023-10-11 10:26:06 +0800

    修复windows和iOS编译错误

--- mono/metadata/icall-def.h
+++ mono/metadata/icall-def.h
@@ -180,7 +180,7 @@ NOHANDLES(ICALL(KPAIR_5, "_ProtectUser", ves_icall_Mono_Security_Cryptography_Ke
 #endif /* !PLATFORM_RO_FS */
 
 ICALL_TYPE(UNITYTLS, "Mono.Unity.UnityTls", UNITYTLS_1)
-ICALL(UNITYTLS_1, "GetUnityTlsInterface", mono_unity_get_unitytls_interface)
+NOHANDLES(ICALL(UNITYTLS_1, "GetUnityTlsInterface", mono_unity_get_unitytls_interface))
 
 ICALL_TYPE(APPDOM, "System.AppDomain", APPDOM_23)
 HANDLES(APPDOM_23, "DoUnhandledException", ves_icall_System_AppDomain_DoUnhandledException, void, 2, (MonoAppDomain, MonoException))
